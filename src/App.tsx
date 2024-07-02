47477c1e49ac625eb0d54f4f0fa1166b1603fd6b
yme <yme@jooble.com>
2024-07-02 22:48:21 +0300

    remove hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import WebApp from '@twa-dev/sdk'
-import useTelegramDataValidation from "./Hooks/useTelegramDataValidation";
 import './App.css';
-
-const BOT_TOKEN = '7401426416:AAHdLBTASLAp9y-kV7JGFK6a_y8F8PpAMC0';
 
 function App() {
   WebApp.ready();
 
- const [ isValid, isRecent, userData ] = useTelegramDataValidation(WebApp.initData, BOT_TOKEN)
-
   return (
       <div className="App">
           <div>
-              {isValid ? (
-                  <div>
-                      <p>Data is valid!</p>
-                      {isRecent ? <p>Data is recent!</p> : <p>Data is outdated.</p>}
-                      <p>User Data:</p>
-                      <pre>{JSON.stringify(userData, null, 2)}</pre>
-                  </div>
-              ) : (
-                  <p>Data validation failed.</p>
-              )}
+              <div>
+                  <p>User Data:</p>
+                  <pre>{WebApp.initData}</pre>
+              </div>
           </div>
       </div>
   );
