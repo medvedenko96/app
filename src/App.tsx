6977fb0e029395ee53e92e3cb40af5d60ac6ce03
yme <yme@jooble.com>
2024-07-02 21:39:49 +0300

    test twa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import WebApp from '@twa-dev/sdk'
+import { MainButton } from '@twa-dev/sdk/react';
 // @ts-ignore
 import logo from './logo.svg';
 import './App.css';
@@ -7,12 +8,38 @@ import './App.css';
 function App() {
   WebApp.ready();
 
+  useEffect(() => {
+    console.log(WebApp.initDataUnsafe);
+    console.log(WebApp.initData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+          </header>
+          <div>
+              {
+                  WebApp.initData && (
+                      <div>
+                          <h3>Init Data</h3>
+                          <pre>{JSON.stringify(WebApp.initData, null, 2)}</pre>
+                      </div>
+                  )
+              }
+          </div>
+          <div>
+              {
+                  WebApp.initDataUnsafe && (
+                      <div>
+                          <h3>Init Data</h3>
+                          <pre>{JSON.stringify(WebApp.initDataUnsafe, null, 2)}</pre>
+                      </div>
+                  )
+              }
+          </div>
+          <MainButton text="Submit" onClick={() => alert('submitted')}/>
+      </div>
   );
 }
 
